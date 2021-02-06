53f3bd72f44b9cd6ae801501b81d4b54e11df9c4
Jordan Boston <jordanboston@users.noreply.github.com>
2021-02-06 12:47:49 -0700

    Update apps.sh
    New brew cask syntax: `brew cask install` is deprecated in favor of `brew install --cask`

--- install/apps.sh
+++ install/apps.sh
@@ -1,42 +1,36 @@
 #!/bin/bash
-brew install homebrew/cask  
 
-brew cask install iterm2
-brew cask install slack
-brew cask install skype
-brew cask install zoom
-brew cask install discord
-brew cask install querious
-brew cask install 1password
-brew cask install 1password-cli
-brew cask install sourcetree
-brew cask install insomnia
-brew cask install postman
-brew cask install android-studio
+brew install --cask iterm2
+brew install --cask slack
+brew install --cask skype
+brew install --cask zoom
+brew install --cask querious
+brew install --cask 1password
+brew install --cask 1password-cli
+brew install --cask sourcetree
+brew install --cask insomnia
+brew install --cask android-studio
 
-brew cask install bettertouchtool
-brew cask install xamarin-profiler
-brew cask install karabiner-elements
-brew cask install visual-studio-code
-brew cask install jetbrains-toolbox
+brew install --cask bettertouchtool
+brew install --cask xamarin-profiler
+brew install --cask karabiner-elements
+brew install --cask visual-studio-code
 
-brew cask install spotify
-brew cask install twitch
+brew install --cask spotify
 
-brew cask install dropbox
-brew cask install amazon-cloud-drive
-brew cask install google-drive
+brew install --cask dropbox
+brew install --cask amazon-cloud-drive
+brew install --cask google-drive
 
-brew cask install adobe-creative-cloud
-brew cask install sip
+brew install --cask adobe-creative-cloud
 
-brew cask install google-chrome
-brew cask install brave-browser
-brew cask install chrome-devtools
-brew cask install opera
-brew cask install firefox
+brew install --cask google-chrome
+brew install --cask brave-browser
+brew install --cask chrome-devtools
+brew install --cask opera
+brew install --cask firefox
 
-brew cask install docker
-brew cask install kitematic
-brew cask install vagrant
-brew cask install virtualbox
+brew install --cask docker
+brew install --cask kitematic
+brew install --cask vagrant
+brew install --cask virtualbox
